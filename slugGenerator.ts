2df8df9f398a5b4fec5705da6a1b518dc05aebc9
Pratik Gurung <pratikgurung022@gmail.com>
2026-08-31 17:40:26 -0700

    Built the DOM based URL cleaner learn 3 properties, getElementByID and how we can import HTMLInputElement for TS to avoid null case and show result in output

--- slugGenerator.ts
+++ slugGenerator.ts
@@ -3,6 +3,19 @@ const slugify = (title: string): string => {
   return cleanedTitle;
 };
 
-let testLine: string = " Hello World My-First Post ";
+/* let testLine: string = " Hello World My-First Post ";
 
-console.log(slugify(testLine));
+console.log(slugify(testLine)); */
+
+const input = document.getElementById("title-input") as HTMLInputElement;
+
+const button = document.getElementById("clean-btn");
+
+const result = document.getElementById("result");
+
+button?.addEventListener("click", () => {
+  if (!input || !result) return;
+  let inpuValue = input.value;
+  const slugValue = slugify(inpuValue);
+  result.textContent = slugValue;
+});
