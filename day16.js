190973bc016588f1a55cebc5db2888f8b8106ee2
Pratik Gurung <pratikgurung022@gmail.com>
2026-09-13 11:04:38 -0700

    raw formatting of textContent while innerText awares the Js in readble format

--- day16.js
+++ day16.js
@@ -1,12 +1,19 @@
 const myDiv = document.getElementById("myDiv");
+const lasso = document.querySelector(".lasso");
+if (myDiv) {
+  console.log(myDiv.dataset);
 
-console.log(myDiv.dataset);
+  myDiv.dataset.name = "Bob";
+  myDiv.dataset.startedCodingAt = 10;
 
-myDiv.dataset.name = "Bob";
-myDiv.dataset.startedCodingAt = 10;
+  //removing actual value
 
-//removing actual value
+  myDiv.dataset.name = "";
 
-myDiv.dataset.name = "";
+  myDiv.removeAttribute("data-name");
+}
+//gets a spacing in HTML format b/c it reads the raw text inside the code editor
+console.log(lasso.textContent);
 
-myDiv.removeAttribute("data-name");
+// is aware of rendered format
+console.log(lasso.innerText);
