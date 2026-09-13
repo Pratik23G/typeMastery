efbe63dce861b3a5c0e7362403dfb6e155950c1c
Pratik Gurung <pratikgurung022@gmail.com>
2026-09-13 11:29:06 -0700

    Back to odin projects function practice before final DOM project

--- day16.js
+++ day16.js
@@ -17,3 +17,31 @@ console.log(lasso.textContent);
 
 // is aware of rendered format
 console.log(lasso.innerText);
+
+//some function testPractice Odin Project
+
+function add7(numberVal) {
+  return numberVal + 7;
+}
+
+console.log(add7(10));
+
+function multiply(firstValue, secondValue) {
+  return firstValue * secondValue;
+}
+
+console.log(multiply(3, 2));
+
+function capitalize(userString) {
+  return userString[0].toUpperCase() + userString.slice(1).toLowerCase();
+}
+
+console.log(capitalize("abcd"));
+console.log(capitalize("ABCD"));
+console.log(capitalize("aBcD"));
+
+function lastletter(userString) {
+  return userString[userString.length - 1];
+}
+
+console.log(lastletter("abcd"));
